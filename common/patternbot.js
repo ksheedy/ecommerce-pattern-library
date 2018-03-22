d62629c5e0b12e26311b9bb7bacc4d396be83d41
ksheedy <shee0067@algonquinlive.com>
2018-03-22 10:14:34 -0400

    Run all patterns through markbot and correct all semantic and grammatical errors

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/katiesheedy/Dropbox/Graphic Design Program/Semester 4/Web Dev/ecommerce-pattern-library
- * @version 1521726155936
+ * @version 1521727775686
  */
-const patternManifest_1521726155936 = {
+const patternManifest_1521727775686 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -995,5 +995,5 @@ const patternManifest_1521726155936 = {
   }
 };
 
-patternBotIncludes(patternManifest_1521726155936);
+patternBotIncludes(patternManifest_1521727775686);
 }());
