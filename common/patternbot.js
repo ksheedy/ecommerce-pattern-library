09fa78f7d0eef98d424b57465f3a58348e74b46c
ksheedy <shee0067@algonquinlive.com>
2018-04-11 17:59:18 -0400

    Fixed sizing of side card in checkout form.

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/katiesheedy/Dropbox/Graphic Design Program/Semester 4/Web Dev/ecommerce-pattern-library
- * @version 1523478907315
+ * @version 1523483724838
  */
-const patternManifest_1523478907315 = {
+const patternManifest_1523483724838 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -1011,5 +1011,5 @@ const patternManifest_1523478907315 = {
   }
 };
 
-patternBotIncludes(patternManifest_1523478907315);
+patternBotIncludes(patternManifest_1523483724838);
 }());
