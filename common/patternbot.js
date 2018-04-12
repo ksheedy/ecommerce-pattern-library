90c2bcac78d3b208f2a917492ef68bda818d63bc
ksheedy <shee0067@algonquinlive.com>
2018-04-12 07:33:22 -0400

    Correct semantic errors in checkout page

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/katiesheedy/Dropbox/Graphic Design Program/Semester 4/Web Dev/ecommerce-pattern-library
- * @version 1523532454594
+ * @version 1523532775635
  */
-const patternManifest_1523532454594 = {
+const patternManifest_1523532775635 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -1011,5 +1011,5 @@ const patternManifest_1523532454594 = {
   }
 };
 
-patternBotIncludes(patternManifest_1523532454594);
+patternBotIncludes(patternManifest_1523532775635);
 }());
